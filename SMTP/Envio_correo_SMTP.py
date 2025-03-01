7798ed80b7b7c90d857c65a67fe19dce3e1c8143
Andres garcia <98360920+AndresGarcia256@users.noreply.github.com>
2025-03-01 01:51:50 -0500

    Update Envio_correo_SMTP.py

--- SMTP/Envio_correo_SMTP.py
+++ SMTP/Envio_correo_SMTP.py
@@ -1,17 +1,22 @@
 import smtplib
 from email.message import EmailMessage
+from dotenv import load_dotenv
+import os
 
-remitente = "Remitente"
+correo = os.getenv('Correo')
+Contrasena = os.getenv('Contrasena')
+
 destinatario = "Destino"
 mensaje = "Hola mundo "
 email = EmailMessage()
-email["From"] = remitente
+email["From"] = correo
 email["To"] = destinatario  
 email["Subject"] = "Correo de prueba"
 email.set_content(mensaje)
+
+
 smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
 smtp.starttls()
-smtp.login(remitente, "Contraseña")
-smtp.sendmail(remitente, destinatario, email.as_string())
+smtp.login(correo, Contrasena)
+smtp.sendmail(correo, destinatario, email.as_string())
 smtp.quit()
-print("correo enviado")
